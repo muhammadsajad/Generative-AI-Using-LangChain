271b7192bcd521d97b6eea3a6d4e5ff7294eac65
Muhammad Sajad <muhammad_sajad47@yahoo.com>
2025-07-21 16:13:17 +0500

    Learn How to create a prompt template in langchain. Date: 21/07/2025

--- langchain-prompts/prompt_ui.py
+++ langchain-prompts/prompt_ui.py
@@ -1,6 +1,7 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 import streamlit as st 
+from langchain_core.prompts import PromptTemplate
 
 load_dotenv()
 
@@ -8,8 +9,44 @@ model=ChatOpenAI()
 
 st.header('Research Tool')
 
-user_input=st.text_input('Enter Your Prompt')
+#user_input=st.text_input('Enter Your Prompt')
 
+
+# Creating selectbox
+paper_input=st.selectbox("Select Research Paper Name",["Attenttion Is All You Need ","BERT: Pre-training of Deep Bidirection Transformers ","GPT-3: Language Models are Few-Shot Learners","Diffusion Models Beat GANs on Image Synthesis"])
+
+style_input=st.selectbox("Select Expanation Style",["Beginner-Friendly","Technical","Code-Oriented","Mathematical"])
+
+length_input=st.selectbox("Select Explanation Length",["Short(1-2 paragraphs)","Medium (3-5 paragraphs)","Long (detailed explanation)"])
+
+
+# Prompt Template
+template=PromptTemplate(
+    template="""
+Please summarize the research paper titled "{paper_input}" with the following specifications:
+Explanation Style: {style_input}
+Explanation Length: {length_input} 
+1. Mathematical Details:  
+   - Include relevant mathematical equations if present in the paper.
+   - Explain the mathematical concepts using simple, intuitive code snippets where applicable.
+   
+2.Analogies:
+   - Use relatable analogies to simplify complex ideas.
+If certain information is not available in the paper, respond with: "Insufficient information available" instead of guessing.  
+Ensure the summary is clear, accurate, and aligned with the provided style and length.
+""",
+input_variables=['paper_input','style_input','length_input']
+)
+
+# fill the placeholders
+prompt=template.invoke({
+    'paper_input':paper_input,
+    'style_input': style_input,
+    'length_input': length_input
+})
+
+#Creating Button
 if st.button('Summarize'):
-    result=model.invoke(user_input)
+    result=model.invoke(prompt)
     st.write(result.content)
+    
