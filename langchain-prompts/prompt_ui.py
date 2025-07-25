3a31f402ea064b5b7f4e2f02104e42b47b592313
Muhammad Sajad <muhammad_sajad47@yahoo.com>
2025-07-25 12:48:21 +0500

    Created a manual chatbot Date: 25/07/2025

--- langchain-prompts/prompt_ui.py
+++ langchain-prompts/prompt_ui.py
@@ -1,7 +1,10 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 import streamlit as st 
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate, load_prompt
+
+# loading the template from json file
+template=load_prompt('template.json')
 
 load_dotenv()
 
@@ -20,33 +23,25 @@ style_input=st.selectbox("Select Expanation Style",["Beginner-Friendly","Technic
 length_input=st.selectbox("Select Explanation Length",["Short(1-2 paragraphs)","Medium (3-5 paragraphs)","Long (detailed explanation)"])
 
 
-# Prompt Template
-template=PromptTemplate(
-    template="""
-Please summarize the research paper titled "{paper_input}" with the following specifications:
-Explanation Style: {style_input}
-Explanation Length: {length_input} 
-1. Mathematical Details:  
-   - Include relevant mathematical equations if present in the paper.
-   - Explain the mathematical concepts using simple, intuitive code snippets where applicable.
-   
-2.Analogies:
-   - Use relatable analogies to simplify complex ideas.
-If certain information is not available in the paper, respond with: "Insufficient information available" instead of guessing.  
-Ensure the summary is clear, accurate, and aligned with the provided style and length.
-""",
-input_variables=['paper_input','style_input','length_input']
-)
 
-# fill the placeholders
-prompt=template.invoke({
-    'paper_input':paper_input,
-    'style_input': style_input,
-    'length_input': length_input
-})
+
+
+
 
 #Creating Button
 if st.button('Summarize'):
-    result=model.invoke(prompt)
+    # Creating chain so that we avoid calling invoke multiple times
+    chain=template|model
+
+    result=chain.invoke(
+        # fill the placeholders
+            {
+        'paper_input':paper_input,
+        'style_input': style_input,
+        'length_input': length_input
+    }
+
+    )
+    #result=model.invoke(prompt)
     st.write(result.content)
     
